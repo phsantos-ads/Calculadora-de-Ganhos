c581b7e725349c92a1bb2b863de5011ebb15a194
Marcos Paulo de Freitas <markimdvp@gmail.com>
2026-09-21 01:00:35 -0300

    refactor: alteracao no nome das variaveis

--- Calculadora-Uber.c
+++ Calculadora-Uber.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 
 int main(){
-    float kmrodado, litros, gastcombustivel, totdia, mediacarro, precombustivel, totliquido, valorkmbruto, valorkmliquido;
+    float kmRodado, litros, gastoCombustivel, totalDia, mediaCarro, preCombustivel, totalLiquido, valorKmBruto, valorKmLiquido;
     int  opcao;
     printf("Qual combustivel foi usado?\n Digite 1-gasolina ou 2-etanol ");
     scanf("%i", &opcao);
@@ -18,19 +18,19 @@ int main(){
         return 0;
     }
         printf("Quantos km voce rodou: \n");
-        scanf("%f", &kmrodado);
+        scanf("%f", &kmRodado);
         printf("Qual a media do carro: \n");
-        scanf("%f", &mediacarro);
+        scanf("%f", &mediaCarro);
         printf("Qual preco do combustivel: \n");
-        scanf("%f", &precombustivel);
+        scanf("%f", &preCombustivel);
         printf("Qual valor final do dia: \n");
-        scanf("%f", &totdia);
-        litros = kmrodado / mediacarro;
-        gastcombustivel = litros * precombustivel;
-        totliquido = totdia - gastcombustivel;
-        valorkmbruto = totdia / kmrodado;
-        valorkmliquido = totliquido / kmrodado;
-        printf("Voce rodou: %.2f KM\ngastou: %.2f reais de combustivel\nobteve valor bruto por km: %.2f \ne valor liquido por km: %.2f \ngerando um lucro liquido de: %.2f\n", kmrodado, gastcombustivel, valorkmbruto, valorkmliquido, totliquido);
+        scanf("%f", &totalDia);
+        litros = kmRodado / mediaCarro;
+        gastoCombustivel = litros * preCombustivel;
+        totalLiquido = totalDia - gastoCombustivel;
+        valorKmBruto = totalDia / kmRodado;
+        valorKmLiquido = totalLiquido / kmRodado;
+        printf("Voce rodou: %.2f KM\ngastou: %.2f reais de combustivel\nobteve valor bruto por km: %.2f \ne valor liquido por km: %.2f \ngerando um lucro liquido de: %.2f\n", kmRodado, gastoCombustivel, valorKmBruto, valorKmLiquido, totalLiquido);
     return 0;
 
     
