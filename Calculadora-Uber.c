5e41691c9519f9774ea13b73f842e2439733e3da
phsantos-ads <phsantos.henrique.96@gmail.com>
2026-09-20 21:15:31 -0300

    Adicionando escolha de combustivel com switch

--- Calculadora-Uber.c
+++ Calculadora-Uber.c
@@ -1,23 +1,37 @@
 #include<stdio.h>
 
 int main(){
-    float kmrodado, litros, gastcombustivel, totdia, mediacarro, precogasosa, totliquido, valorkmbruto, valorkmliquido;
-    printf("Quantos km voce rodou: \n");
-    scanf("%f", &kmrodado);
-    printf("Qual a media do carro: \n");
-    scanf("%f", &mediacarro);
-    printf("Qual preco da gasolina: \n");
-    scanf("%f", &precogasosa);
-    printf("Qual valor final do dia: \n");
-    scanf("%f", &totdia);
-    litros = kmrodado / mediacarro;
-    gastcombustivel = litros * precogasosa;
-    totliquido = totdia - gastcombustivel;
-    valorkmbruto = totdia / kmrodado;
-    valorkmliquido = totliquido / kmrodado;
-    printf("Voce rodou: %.2f KM\ngastou: %.2f reais de combustivel\nobteve kmbruto: %.2f \ne kmliquido: %.2f \ngerando um lucro luquido de: %.2f\n", kmrodado, gastcombustivel, valorkmbruto, valorkmliquido, totliquido);
-
+    float kmrodado, litros, gastcombustivel, totdia, mediacarro, precombustivel, totliquido, valorkmbruto, valorkmliquido;
+    int  opcao;
+    printf("Qual combustivel foi usado?\n Digite 1-gasolina ou 2-etanol ");
+    scanf("%i", &opcao);
+    switch (opcao)
+    {
+    case 1:
+  
+        break;
+    case 2:    
+        
+        break;
+    default:
+        printf("\nOpcao invalida, digite 1 ou 2");
+        return 0;
+    }
+        printf("Quantos km voce rodou: \n");
+        scanf("%f", &kmrodado);
+        printf("Qual a media do carro: \n");
+        scanf("%f", &mediacarro);
+        printf("Qual preco do combustivel: \n");
+        scanf("%f", &precombustivel);
+        printf("Qual valor final do dia: \n");
+        scanf("%f", &totdia);
+        litros = kmrodado / mediacarro;
+        gastcombustivel = litros * precombustivel;
+        totliquido = totdia - gastcombustivel;
+        valorkmbruto = totdia / kmrodado;
+        valorkmliquido = totliquido / kmrodado;
+        printf("Voce rodou: %.2f KM\ngastou: %.2f reais de combustivel\nobteve valor bruto por km: %.2f \ne valor liquido por km: %.2f \ngerando um lucro liquido de: %.2f\n", kmrodado, gastcombustivel, valorkmbruto, valorkmliquido, totliquido);
     return 0;
 
-
+    
 }
